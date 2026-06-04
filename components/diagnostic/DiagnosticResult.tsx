e835cf8f2eae9ae448b7806b736dc9e13671c280
AudreyFeathersDigitale <feathers.digitale@gmail.com>
2026-06-04 22:47:05 +0200

    mini agent diag grandeur photo3

--- components/diagnostic/DiagnosticResult.tsx
+++ components/diagnostic/DiagnosticResult.tsx
@@ -32,7 +32,7 @@ export default function DiagnosticResult({ profile, prenom }: Props) {
         <img
   src="/hari-cutout.png"
   alt=""
-  className="absolute bottom-[-60px] left-[-45px] z-20 h-[200%] max-w-none object-contain"
+  className="absolute bottom-[0px] left-[-45px] z-20 h-[15s0%] max-w-none object-contain"
 />
 
         {/* Contenu */}
